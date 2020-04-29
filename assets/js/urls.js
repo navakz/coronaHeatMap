67b577a73f9f5780ada75e9fb69b8e63a37a91cc
navakz <naveen.swayamvarpu@gmail.com>
2020-04-29 19:33:57 +0530

    v2.0

---
+++ assets/js/urls.js
@@ -0,0 +1,6 @@
+const allCasesURL = "https://corona.lmao.ninja/v2/all";
+const countryWiseCasesURL = "https://corona.lmao.ninja/v2/countries?sort=country";
+const globalCasesURL = "https://corona.lmao.ninja/v2/jhucsse";
+const travelHistoryIndiaCasesURL = "https://api.covid19india.org/travel_history.json";
+const rawDataURL = "https://api.covid19india.org/raw_data.json";
+const timeSeriesURL = "https://api.covid19india.org/data.json";
